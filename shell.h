e7f109171ea16d6826657bf01722706bddd42f38
IanAvi15 <icaviles20@hotmail.com>
2026-04-13 21:35:49 -0400

    adding header file

--- shell.h
+++ shell.h
@@ -1,1 +1,23 @@
-shell file 
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+extern char **environ;
+
+/* main shell functions */
+void prompt(void);
+char *read_line(void);
+char **split_line(char *line);
+int execute(char **args, char *argv0, int count);
+char *find_path(char *cmd);
+void free_args(char **args);
+void print_error(char *argv0, int count, char *cmd);
+
+#endif /* SHELL_H */
